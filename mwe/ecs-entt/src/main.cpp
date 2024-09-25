72d499c09e7b9b02df5ce93a44dc9a6ff66bf847
Max-001 <80035972+Max-001@users.noreply.github.com>
2024-09-25 10:59:08 +0200

    Added first real EnTT tests

--- mwe/ecs-entt/src/main.cpp
+++ mwe/ecs-entt/src/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
-#include <entt/entity/registry.hpp>
+#include <entt/entt.hpp>
+
+class TestComponent {
+public:
+	TestComponent() {}
+	TestComponent(int variable) {
+		mVariable = variable;
+	}
+
+	void print() {
+		std::cout << "TestComponent variable: " << mVariable << std::endl;
+	}
+
+private:
+	int mVariable = 0;
+};
 
 int main() {
-	std::cout << "Hello, world!" << std::endl;
+	entt::registry registry;	//The registry stores/manages all the entities and its components (it's a container) (just like the component manager described in the research document)
 
-	entt::registry registry;
+	entt::entity entity0 = registry.create();	//This creates an entity (be aware that an entity is nothing more than a std::uint32_t)
+												//Be aware that "entity.somthing()" is not possible, because an entity is just a std::uint32_t
+	entt::entity entity1 = registry.create();	//Add a second entity to the registry
+
+	registry.emplace<TestComponent>(entity0);	//This adds the TestComponent component to entity0
+	registry.emplace<TestComponent>(entity1, 1);	//TestComponent also has a non-default constructor. In the case of a constructor with arguments, you would just type "registry.emplace<TestComponent>(entity, argument1, argument2, etc.);"
+
+	if(registry.all_of<TestComponent>(entity0)) {	//Returns true if TestComponent is (one of) the component(s) of the entity
+		TestComponent& testComponent = registry.get<TestComponent>(entity0);	//Gets the reference to the TestComponent component of this entity
+    	testComponent.print();	//Do something with the component
+	} else {
+    	std::cout << "Entity does not have a TestComponent!" << std::endl;
+	}
+
+	auto view = registry.view<TestComponent>();	//This returns all entities that have the TestComponent component
+	for(entt::entity entity : view) {	//Loop through all entities that have the TestComponent component
+		TestComponent& testComponent = view.get<TestComponent>(entity);	//Get the reference to the TestComponent component of this entity
+		testComponent.print();	//Do something with the component
+	}
 
 	return 0;
 }
