9cbd4ccc59795b503f3240dad532d19121920914
Max-001 <80035972+Max-001@users.noreply.github.com>
2024-09-25 13:23:39 +0200

    Tried to use EnTT library as desribed in the class diagram (as much as possible)

--- mwe/ecs-entt/src/main.cpp
+++ mwe/ecs-entt/src/main.cpp
@@ -1,43 +1,99 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
 #include <entt/entt.hpp>
 
-class TestComponent {
+class Component {
 public:
-	TestComponent() {}
-	TestComponent(int variable) {
-		mVariable = variable;
+	Component() : mActive(true) {}
+	virtual ~Component() {}
+
+	bool mActive;
+};
+
+class Sprite : Component {
+public:
+	void render() {
+		std::cout << "Rendering sprite" << std::endl;
+	}
+};
+
+class Rigidboy : Component {
+public:
+	Rigidboy(int mass, int gravityScale, int bodyType) : mMass(mass), mGravityScale(gravityScale), mBodyType(bodyType) {}
+
+	int mMass;
+	int mGravityScale;
+	int mBodyType;
+};
+
+class Systems {
+public:
+	Systems(entt::registry& registry) : mRegistry(registry) {}
+
+	void spriteSystem() {
+		auto view = mRegistry.view<Sprite>();
+		for(entt::entity gameObject : view) {
+			Sprite& sprite = view.get<Sprite>(gameObject);
+			std::cout << "Current GameObject ID: " << (uint32_t)gameObject << std::endl;
+			sprite.render();
+			std::cout << std::endl;
+		}
 	}
 
-	void print() {
-		std::cout << "TestComponent variable: " << mVariable << std::endl;
+	void rigidbodySystem() {
+		auto view = mRegistry.view<Rigidboy>();
+		for(entt::entity gameObject : view) {
+			Rigidboy& rigidbody = view.get<Rigidboy>(gameObject);
+			std::cout << "Current GameObject ID: " << (uint32_t)gameObject << std::endl;
+			std::cout << rigidbody.mMass << " " << rigidbody.mGravityScale << " " << rigidbody.mBodyType << std::endl;
+			std::cout << std::endl;
+		}
 	}
 
 private:
-	int mVariable = 0;
+	entt::registry& mRegistry;
+};
+
+class Scene {
+public:
+	Scene() : systems(mRegistry) {}
+
+	void createScene() {
+		addGameObject();
+		addGameObject();
+
+		addComponentToGameObject<Sprite>(mGameObjects[0]);
+		addComponentToGameObject<Rigidboy>(mGameObjects[0], 10, 10, 5);
+		addComponentToGameObject<Rigidboy>(mGameObjects[1], 20, 100, 25);
+	}
+
+	void runSystems() {
+		systems.spriteSystem();
+		systems.rigidbodySystem();
+	}
+
+private:
+	void addGameObject() {
+		mGameObjects.push_back(mRegistry.create());
+	}
+
+	template <typename T, typename... Args>
+	void addComponentToGameObject(entt::entity gameObject, Args&&... args) {
+    	mRegistry.emplace<T>(gameObject, std::forward<Args>(args)...);
+	}
+
+	std::vector<entt::entity> mGameObjects;
+	entt::registry mRegistry;
+	Systems systems;
 };
 
 int main() {
-	entt::registry registry;	//The registry stores/manages all the entities and its components (it's a container) (just like the component manager described in the research document)
+	Scene scene;
 
-	entt::entity entity0 = registry.create();	//This creates an entity (be aware that an entity is nothing more than a std::uint32_t)
-												//Be aware that "entity.somthing()" is not possible, because an entity is just a std::uint32_t
-	entt::entity entity1 = registry.create();	//Add a second entity to the registry
-
-	registry.emplace<TestComponent>(entity0);	//This adds the TestComponent component to entity0
-	registry.emplace<TestComponent>(entity1, 1);	//TestComponent also has a non-default constructor. In the case of a constructor with arguments, you would just type "registry.emplace<TestComponent>(entity, argument1, argument2, etc.);"
-
-	if(registry.all_of<TestComponent>(entity0)) {	//Returns true if TestComponent is (one of) the component(s) of the entity
-		TestComponent& testComponent = registry.get<TestComponent>(entity0);	//Gets the reference to the TestComponent component of this entity
-    	testComponent.print();	//Do something with the component
-	} else {
-    	std::cout << "Entity does not have a TestComponent!" << std::endl;
-	}
-
-	auto view = registry.view<TestComponent>();	//This returns all entities that have the TestComponent component
-	for(entt::entity entity : view) {	//Loop through all entities that have the TestComponent component
-		TestComponent& testComponent = view.get<TestComponent>(entity);	//Get the reference to the TestComponent component of this entity
-		testComponent.print();	//Do something with the component
-	}
+	scene.createScene();
+	scene.runSystems();
 
 	return 0;
 }
