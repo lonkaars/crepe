3cb7227c3c9678141ff74915331b706265c380cb
Loek Le Blansch <loek@pipeframe.xyz>
2024-09-28 17:07:56 +0200

    more WIP audio facade

--- src/crepe/Sound.cpp
+++ src/crepe/Sound.cpp
@@ -1,9 +1,24 @@
 #include "Sound.h"
+#include "SoundSystem.h"
 
 using namespace crepe;
 
-Sound::Sound(std::unique_ptr<api::Resource> res) {
-	_res = std::move(res);
+Sound::Sound(std::unique_ptr<api::Resource> res, SoundSystem & system) : system(system) {
+	this->res = std::move(res);
 }
 
+void Sound::play() {
+	if (this->system.engine.getPause(this->handle)) {
+		// resume if paused
+		this->system.engine.setPause(this->handle, false);
+	} else {
+		// or start new sound
+		this->handle = this->system.engine.play(this->sample);
+	}
+}
+
+void Sound::pause() {
+	if (this->system.engine.getPause(this->handle)) return;
+	this->system.engine.setPause(this->handle, true);
+}
 
--- src/crepe/api/Resource.h
+++ src/crepe/api/Resource.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <iostream>
 
 namespace crepe::api {
 
 class Resource {
 public:
-	Resource(const std::string & source);
-	virtual ~Resource();
+	Resource(const std::string & src);
+
+public:
+	const std::istream & read();
 
 private:
-	std::string _source;
+	std::string src;
+	std::ifstream file;
 };
 
 }
