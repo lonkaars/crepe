f917d34e350a49ce17088e17842520e725d606c5
Max-001 <80035972+Max-001@users.noreply.github.com>
2024-10-02 16:37:47 +0200

    Added DeleteComponentsById<> method

--- mwe/ecs-homemade/src/main.cpp
+++ mwe/ecs-homemade/src/main.cpp
@@ -69,8 +69,8 @@ int main() {
 	}
 	std::cout << std::endl;
 
-	ComponentManager::GetInstance().DeleteComponents<Sprite>();
-	gameObect5.AddComponent<Sprite>();
+	ComponentManager::GetInstance().DeleteComponentsById<Sprite>(gameObect2.mId);
+	gameObect2.AddComponent<Sprite>();
 
 	std::cout << "Finding all rigidbodies of entity 3" << std::endl;
 	std::vector<std::reference_wrapper<Rigidbody>> rigidbodyOfEntity3 = ComponentManager::GetInstance().GetComponentsByID<Rigidbody>(gameObect3.mId);
