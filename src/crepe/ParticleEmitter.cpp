5cf5cf64d6f9597849ed5558da4f1bc201165fed
jaroWMR <jarorutjes07@gmail.com>
2024-10-07 16:39:26 +0200

    updated system

--- src/crepe/ParticleEmitter.cpp
+++ src/crepe/ParticleEmitter.cpp
@@ -1,8 +1,8 @@
 #include "ParticleEmitter.hpp"
 #include <ctime>
 
-ParticleEmitter::ParticleEmitter(unsigned int maxParticles, unsigned int emissionRate, unsigned int speed, unsigned int speedOffset, unsigned int angle, unsigned int angleOffset)
-    : m_maxParticles(maxParticles), m_emissionRate(emissionRate), m_speed(speed), m_speedOffset(speedOffset), m_position{0, 0} {
+ParticleEmitter::ParticleEmitter(unsigned int maxParticles, unsigned int emissionRate, unsigned int speed, unsigned int speedOffset, unsigned int angle, unsigned int angleOffset, float m_beginLifespan, float m_endLifespan)
+    : m_maxParticles(maxParticles), m_emissionRate(emissionRate), m_speed(speed), m_speedOffset(speedOffset), m_position{0, 0}, m_beginLifespan(m_beginLifespan),m_endLifespan(m_endLifespan) {
     std::srand(static_cast<unsigned int>(std::time(nullptr))); // initialize random seed
 
     m_minAngle = (360 + angle - (angleOffset % 360)) % 360; // calculate minAngle
