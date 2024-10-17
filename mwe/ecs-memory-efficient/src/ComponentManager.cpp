61becc23cd67f94e2dfa1ad9e283e79b87749620
Loek Le Blansch <loek@pipeframe.xyz>
2024-10-17 17:12:27 +0200

    `make format`

--- mwe/ecs-memory-efficient/src/ComponentManager.cpp
+++ mwe/ecs-memory-efficient/src/ComponentManager.cpp
@@ -2,20 +2,21 @@
 
 ComponentManager ComponentManager::mInstance;
 
-ComponentManager& ComponentManager::GetInstance() {
-	return mInstance;
-}
+ComponentManager & ComponentManager::GetInstance() { return mInstance; }
 
 ComponentManager::ComponentManager() {}
 
-std::vector<std::reference_wrapper<Sprite>> ComponentManager::getAllSpriteReferences() {
+std::vector<std::reference_wrapper<Sprite>>
+ComponentManager::getAllSpriteReferences() {
 	return mSpriteContainer.getAllReferences();
 }
 
-std::vector<std::reference_wrapper<Rigidbody>> ComponentManager::getAllRigidbodyReferences() {
+std::vector<std::reference_wrapper<Rigidbody>>
+ComponentManager::getAllRigidbodyReferences() {
 	return mRigidbodyContainer.getAllReferences();
 }
 
-std::vector<std::reference_wrapper<Colider>> ComponentManager::getAllColiderReferences() {
+std::vector<std::reference_wrapper<Colider>>
+ComponentManager::getAllColiderReferences() {
 	return mColiderContainer.getAllReferences();
 }
