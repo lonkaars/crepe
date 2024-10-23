de356f60b91fab37b1456992dc66ada8bd8e4dd7
jaroWMR <jarorutjes07@gmail.com>
2024-10-23 20:06:28 +0200

    fixed merge issue

--- src/example/Physics.cpp
+++ src/example/Physics.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include "Transform.h"
+#include <crepe/api/Transform.h>
 #include <crepe/api/Rigidbody.h>
 #include <crepe/api/Force.h>
 #include "PhysicsSystem.h"
@@ -19,8 +19,11 @@ int main(int argc, char* argv[]) {
 	GameObject * game_object[2];
 	game_object[1] = new GameObject(2, "Name", "Tag", 0); // not found not used
 	game_object[0] = new GameObject(5, "Name", "Tag", 0);
-	Position position = {0, 0};
-	game_object[0]->add_component<Transform>(position,0,0);
+	Point point = {
+			.x = 0,
+			.y = 0,
+		};
+	game_object[0]->add_component<Transform>(point,0,0);
 	game_object[0]->add_component<Rigidbody>(1, 1 , BodyType::Dynamic);
 	game_object[0]->add_component<Force>(1 , 0);
 	physicsSystem.update(); 
