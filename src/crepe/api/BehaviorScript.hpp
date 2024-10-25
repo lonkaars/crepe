195f7770c42aa1ed1052725dde26699d2ecc689f
Loek Le Blansch <loek@pipeframe.xyz>
2024-10-25 20:46:32 +0200

    implement get_component for scripts

--- src/crepe/api/BehaviorScript.hpp
+++ src/crepe/api/BehaviorScript.hpp
@@ -5,6 +5,7 @@
 #include "../util/log.h"
 
 #include "BehaviorScript.h"
+#include "Script.h"
 
 namespace crepe::api {
 
@@ -13,6 +14,7 @@ BehaviorScript & BehaviorScript::set_script() {
 	static_assert(std::is_base_of<Script, T>::value);
 	dbg_trace();
 	Script * s = new T();
+	s->parent = this;
 	this->script = std::unique_ptr<Script>(s);
 	return *this;
 }
---
+++ src/crepe/api/Script.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../ComponentManager.h"
+
+#include "Script.h"
+#include "BehaviorScript.h"
+
+namespace crepe::api {
+
+template<typename T>
+T & Script::get_component() {
+	std::vector<std::reference_wrapper<T>> all_components = this->get_components<T>();
+	if (all_components.size() < 1)
+		throw nullptr; // TODO
+	
+	return all_components.back().get();
+}
+
+template<typename T>
+std::vector<std::reference_wrapper<T>> Script::get_components() {
+	ComponentManager & mgr = ComponentManager::get_instance();
+	return mgr.get_components_by_id<T>(this->parent->game_object_id);
+}
+
+}
+
