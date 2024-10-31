98b215abc2e1ead954b83481ce21c5b14c1004fe
Loek Le Blansch <loek@pipeframe.xyz>
2024-10-31 18:40:45 +0100

    minor script system fixes

--- src/example/script.cpp
+++ src/example/script.cpp
@@ -17,27 +17,38 @@ using namespace crepe;
 using namespace crepe::api;
 using namespace std;
 
+// Unrelated stuff that is not part of this POC
+int _ = [] () {
+	// Show dbg_trace() output
+	auto & cfg = api::Config::get_instance();
+	cfg.log.level = util::LogLevel::TRACE;
+
+	return 0; // satisfy compiler
+}();
+
+
+
+// User-defined script:
 class MyScript : public Script {
 	void update() {
+		// Retrieve component from the same GameObject this script is on
 		Transform & test = get_component<Transform>();
 		dbg_logf("Transform(%.2f, %.2f)", test.position.x, test.position.y);
 	}
 };
 
 int main() {
-	auto & cfg = api::Config::get_instance();
-	cfg.log.level = util::LogLevel::TRACE;
-
+	// Create game object with Transform and BehaviorScript components
 	auto obj = GameObject(0, "name", "tag", 0);
-	Point point = {
-		.x = 1.2,
-		.y = 3.4,
-	};
-	obj.add_component<Transform>(point, 0, 0);
+	obj.add_component<Transform>(Point { .x = 1.2, .y = 3.4, }, 0, 0);
 	obj.add_component<BehaviorScript>().set_script<MyScript>();
 
+	// Get ScriptSystem singleton instance (this would normally be done from the
+	// game loop)
 	auto & sys = ScriptSystem::get_instance();
-	sys.update(); // -> MyScript::update
+	// Update all scripts. This should result in MyScript::update being called
+	sys.update();
 
-	return 0;
+	return EXIT_SUCCESS;
 }
+
