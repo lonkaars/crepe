761330ff6ee1febb0ecb223b6244416248b7f894
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-04 08:23:51 +0100

    finish get/set implementations on DB

--- src/crepe/DB.h
+++ src/crepe/DB.h
@@ -20,14 +20,14 @@ public:
 public:
 	std::string get(const std::string & key);
 	void set(const std::string & key, const std::string & value);
-	bool has(const std::string & key);
+	bool has(const std::string & key) noexcept;
 
 private:
 	std::unique_ptr<libdb::DB, std::function<void(libdb::DB *)>> db;
 	std::unique_ptr<libdb::DBC, std::function<void(libdb::DBC *)>> cursor;
 
 private:
-	libdb::DBT to_thing(const std::string & thing);
+	libdb::DBT to_thing(const std::string & thing) const;
 };
 
 }
