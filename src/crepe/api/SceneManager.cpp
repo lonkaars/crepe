d478c8b76d6155bd9653f81787f73db07c55d9f3
max-001 <maxsmits21@kpnmail.nl>
2024-11-05 16:12:10 +0100

    Added SceneManager

---
+++ src/crepe/api/SceneManager.cpp
@@ -0,0 +1,36 @@
+#include "../ComponentManager.h"
+
+#include "SceneManager.h"
+
+using namespace crepe::api;
+
+SceneManager::SceneManager() {}
+
+SceneManager & SceneManager::get_instance() {
+	static SceneManager instance;
+	return instance;
+}
+
+// Push the next scene onto the queue
+void SceneManager::load_scene(std::string name) {
+	next_scene.push(name);
+}
+
+// Load a new scene from the queue (if there is one)
+void SceneManager::empty_queue() {
+	while (!next_scene.empty()) {
+		string name = next_scene.front();
+		next_scene.pop();
+
+		for (auto & scene : scenes) {
+			if (scene->name == name) {
+				// Delete all components of the current scene
+				ComponentManager & mgr = ComponentManager::get_instance();
+				mgr.delete_all_components();
+				// Load the new scene
+				scene->load_scene();
+				break;
+			}
+		}
+	}
+}
---
+++ src/crepe/api/SceneManager.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <queue>
+
+#include "Scene.h"
+
+namespace crepe::api {
+
+class SceneManager {
+public:
+	// Singleton
+	static SceneManager & get_instance();
+	SceneManager(const SceneManager &) = delete;
+	SceneManager(SceneManager &&) = delete;
+	SceneManager & operator=(const SceneManager &) = delete;
+	SceneManager & operator=(SceneManager &&) = delete;
+
+public:
+	template <typename T>
+	void add_scene(std::string name);
+	void load_scene(std::string name);
+	void empty_queue();
+
+private:
+	SceneManager();
+
+private:
+	std::vector<std::unique_ptr<Scene>> scenes;
+	std::queue<std::string> next_scene;
+};
+
+} // namespace crepe::api
+
+#include "SceneManager.hpp"
