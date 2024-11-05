5b8ec02b6b4f2356e2d688502298274e372b9fad
max-001 <maxsmits21@kpnmail.nl>
2024-11-05 13:57:49 +0100

    Make format

--- src/crepe/Metadata.cpp
+++ src/crepe/Metadata.cpp
@@ -3,8 +3,7 @@
 using namespace crepe;
 using namespace std;
 
-Metadata::Metadata(uint32_t gameObjectId, string name, string tag) : Component(gameObjectId), name(name), tag(tag) {}
+Metadata::Metadata(uint32_t gameObjectId, string name, string tag)
+	: Component(gameObjectId), name(name), tag(tag) {}
 
-int Metadata::get_instances_max() const {
-	return 1;
-}
+int Metadata::get_instances_max() const { return 1; }
--- src/crepe/api/GameObject.cpp
+++ src/crepe/api/GameObject.cpp
@@ -1,12 +1,14 @@
 #include "api/Transform.h"
 
-#include "Metadata.h"
 #include "GameObject.h"
+#include "Metadata.h"
 
 using namespace crepe::api;
 using namespace std;
 
-GameObject::GameObject(uint32_t id, std::string name, std::string tag, Point position, double rotation, double scale) : id(id) {
+GameObject::GameObject(uint32_t id, std::string name, std::string tag,
+					   Point position, double rotation, double scale)
+	: id(id) {
 	ComponentManager & mgr = ComponentManager::get_instance();
 	mgr.add_component<Transform>(this->id, position, rotation, scale);
 	mgr.add_component<Metadata>(this->id, name, tag);
@@ -14,8 +16,10 @@ GameObject::GameObject(uint32_t id, std::string name, std::string tag, Point pos
 
 void GameObject::set_parent(GameObject & parent) {
 	auto & mgr = ComponentManager::get_instance();
-	vector<reference_wrapper<Metadata>> thisMetadata = mgr.get_components_by_id<Metadata>(this->id);
-	vector<reference_wrapper<Metadata>> parentMetadata = mgr.get_components_by_id<Metadata>(parent.id);
+	vector<reference_wrapper<Metadata>> thisMetadata
+		= mgr.get_components_by_id<Metadata>(this->id);
+	vector<reference_wrapper<Metadata>> parentMetadata
+		= mgr.get_components_by_id<Metadata>(parent.id);
 	thisMetadata.at(0).get().parent = parent.id;
 	parentMetadata.at(0).get().children.push_back(this->id);
 }
