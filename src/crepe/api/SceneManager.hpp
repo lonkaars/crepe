157ab99df031bc4ff3a78301efd1bf10955bb3f1
max-001 <maxsmits21@kpnmail.nl>
2024-11-05 16:12:10 +0100

    Added SceneManager

---
+++ src/crepe/api/SceneManager.hpp
@@ -0,0 +1,11 @@
+#include "SceneManager.h"
+
+using namespace crepe::api;
+using namespace std;
+
+// Add a new concrete scene to the scene manager
+template <typename T>
+void SceneManager::add_scene(string name) {
+	static_assert(is_base_of<Scene, T>::value, "T must be derived from Scene");
+	scenes.emplace_back(make_unique<T>(name));
+}
