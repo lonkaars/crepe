7cf5fed595e815fbdd0f9c7b5673554b921ed712
max-001 <maxsmits21@kpnmail.nl>
2024-11-05 12:02:04 +0100

    Added Matadata component

---
+++ src/crepe/Metadata.cpp
@@ -0,0 +1,10 @@
+#include "Metadata.h"
+
+using namespace crepe;
+using namespace std;
+
+Metadata::Metadata(uint32_t gameObjectId, string name, string tag) : Component(gameObjectId), name(name), tag(tag) {}
+
+int Metadata::get_instances_max() const {
+	return 1;
+}
