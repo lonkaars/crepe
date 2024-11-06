3a5201961ce31d415042c6273d03e46aed7e6eb8
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-06 18:38:45 +0100

    fix code standard and merge #19

--- src/crepe/api/SceneManager.h
+++ src/crepe/api/SceneManager.h
@@ -18,13 +18,27 @@ public:
 	SceneManager & operator=(SceneManager &&) = delete;
 
 public:
+	/**
+	 * \brief Add a new concrete scene to the scene manager
+	 *
+	 * \tparam T  Type of concrete scene
+	 * \param name  Name of new scene
+	 */
 	template <typename T>
 	void add_scene(const std::string & name);
+	/**
+	 * \brief Set the next scene
+	 *
+	 * This scene will be loaded at the end of the frame
+	 *
+	 * \param name  Name of the next scene
+	 */
 	void set_next_scene(const std::string & name);
+	//! Load a new scene (if there is one)
 	void load_next_scene();
 
 private:
-	SceneManager();
+	SceneManager() = default;
 
 private:
 	std::vector<std::unique_ptr<Scene>> scenes;
--- src/crepe/api/SceneManager.hpp
+++ src/crepe/api/SceneManager.hpp
@@ -2,11 +2,11 @@
 
 namespace crepe {
 
-// Add a new concrete scene to the scene manager
 template <typename T>
 void SceneManager::add_scene(const std::string & name) {
 	static_assert(std::is_base_of<Scene, T>::value,
 				  "T must be derived from Scene");
+
 	scenes.emplace_back(make_unique<T>(name));
 
 	// The first scene added, is the one that will be loaded at the beginning
