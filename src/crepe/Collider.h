a1ca2adb9f86dc164be09f199c01ffa6113895e4
JAROWMR <jarorutjes07@gmail.com>
2024-11-10 19:49:10 +0100

    created colliders

--- src/crepe/Collider.h
+++ src/crepe/Collider.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "Component.h"
+#include "api/Vector2.h"
 
 namespace crepe {
 
 class Collider : public Component {
 public:
-	Collider(game_object_id_t id);
-
-	int size;
+	Collider(game_object_id_t id, Vector2 offset);
+	Vector2 offset;
 };
 
 } // namespace crepe
---
+++ src/crepe/api/CircleCollider.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "CircleCollider.h"
+
+using namespace crepe;
+
+
+CircleCollider::CircleCollider(game_object_id_t game_object_id,Vector2 offset, int radius) : Collider(game_object_id,offset), radius(radius) {}
