cf96d9639dbd18df6451a07fd7b623616e842c4a
heavydemon21 <nielsstunnebrink1@gmail.com>
2024-11-11 21:27:54 +0100

    adjusted rendering and example

--- src/example/rendering_particle.cpp
+++ src/example/rendering_particle.cpp
@@ -21,17 +21,17 @@ using namespace crepe;
 using namespace std;
 
 int main(int argc, char * argv[]) {
-	GameObject game_object(0, "", "", Vector2{0, 0}, 0, 0.1);
+	GameObject game_object(0, "", "", Vector2{100, 0}, 0, 0.1);
 	Color color(0, 0, 0, 0);
 	Sprite test_sprite = game_object.add_component<Sprite>(
 		make_shared<Texture>("../asset/texture/img.png"), color,
 		FlipSettings{false, false});
 	game_object.add_component<ParticleEmitter>(ParticleEmitter::Data{
 		.position = {0, 0},
-		.max_particles = 100,
-		.emission_rate = 0.5,
-		.min_speed = 1,
-		.max_speed = 1,
+		.max_particles = 10,
+		.emission_rate = 0.1,
+		.min_speed = 10,
+		.max_speed = 10,
 		.min_angle = 0,
 		.max_angle = 0,
 		.begin_lifespan = 0,
