da3b00d44fc1aa48b469fad75a4ccdd03a2c3a78
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-12 18:26:59 +0100

    `make format`

--- src/crepe/api/Transform.cpp
+++ src/crepe/api/Transform.cpp
@@ -6,6 +6,9 @@ using namespace crepe;
 
 Transform::Transform(game_object_id_t id, const Vector2 & point,
 					 double rotation, double scale)
-	: Component(id), position(point), rotation(rotation), scale(scale) {
+	: Component(id),
+	  position(point),
+	  rotation(rotation),
+	  scale(scale) {
 	dbg_trace();
 }
