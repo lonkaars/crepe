da478cf4c71f70d3b10ec314f83279a5213d784a
WBoerenkamps <wrj.boerenkamps@student.avans.nl>
2024-11-12 15:08:37 +0100

    removed auto from code

--- src/crepe/api/IKeyListener.h
+++ src/crepe/api/IKeyListener.h
@@ -4,18 +4,22 @@
 #include "EventManager.h"
 class IKeyListener {
 public:
+	IKeyListener(int channel);
 	IKeyListener();
     virtual ~IKeyListener();
     virtual bool on_key_pressed(const KeyPressEvent & event) = 0;
     virtual bool on_key_released(const KeyReleaseEvent & event) = 0;
+	void activate_keys(int listenerId = 0);
+    void deactivate_keys();
 
 protected:
     void subscribe_events(int listenerId = 0);
     void unsubscribe_events(int listenerId = 0);
-    void activate(int listenerId = 0) { subscribe_events(listenerId); }
-    void deactivate(int listenerId = 0) { unsubscribe_events(listenerId); }
+    
 
 private:
+	bool active = true;
+	int channel = 0;
     EventHandler<KeyPressEvent> key_pressed_handler;
     EventHandler<KeyReleaseEvent> key_released_handler;
 };
