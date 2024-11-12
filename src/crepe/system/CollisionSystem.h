116972b76ad700ef030cb961b0ce394239837f50
JAROWMR <jarorutjes07@gmail.com>
2024-11-12 16:09:43 +0100

    added collision methodes

--- src/crepe/system/CollisionSystem.h
+++ src/crepe/system/CollisionSystem.h
@@ -16,10 +16,11 @@ public:
 	CollisionSystem();
 	void update();
 private:
-	bool check_collisions(const std::vector<Collider*>& colliders1, const std::vector<Collider*>& colliders2);
+  void check_collisions(const std::vector<std::reference_wrapper<BoxCollider>>& boxcolliders, const std::vector<std::reference_wrapper<CircleCollider>>& circlecolliders) ;
 	bool check_box_box_collision(const BoxCollider& box1, const BoxCollider& box2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
-	bool check_box_circle_collision(const BoxCollider& box, const CircleCollider& circle);
-	bool check_circle_circle_collision(const CircleCollider& circle1, const CircleCollider& circle2);
+	bool check_box_circle_collision(const BoxCollider& box1, const CircleCollider& circle2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
+	bool check_circle_circle_collision(const CircleCollider& circle1, const CircleCollider& circle2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
+	Vector2 current_position(const Collider& collider, const Transform& transform, const Rigidbody& rigidbody);
 };
 
 } // namespace crepe
