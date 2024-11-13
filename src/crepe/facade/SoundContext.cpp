4ed22b6358937db10e1d44ece8e55d1e2f46c5bd
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-13 12:33:59 +0100

    move some files from `loek/tests` to `loek/audio`

--- src/crepe/facade/SoundContext.cpp
+++ src/crepe/facade/SoundContext.cpp
@@ -4,6 +4,11 @@
 
 using namespace crepe;
 
+SoundContext & SoundContext::get_instance() {
+	static SoundContext instance;
+	return instance;
+}
+
 SoundContext::SoundContext() {
 	dbg_trace();
 	engine.init();
