4038557ef334a20d52777dedea51a1601c73b8cf
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-13 19:52:11 +0100

    `make format`

--- src/crepe/api/ParticleEmitter.cpp
+++ src/crepe/api/ParticleEmitter.cpp
@@ -2,8 +2,10 @@
 
 using namespace crepe;
 
-ParticleEmitter::ParticleEmitter(game_object_id_t game_object_id, const Data & data)
-	: Component(game_object_id), data(data) {
+ParticleEmitter::ParticleEmitter(game_object_id_t game_object_id,
+								 const Data & data)
+	: Component(game_object_id),
+	  data(data) {
 	for (size_t i = 0; i < this->data.max_particles; i++) {
 		this->data.particles.emplace_back();
 	}
