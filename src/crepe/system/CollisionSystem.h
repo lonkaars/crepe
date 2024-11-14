b58765b671969aa810b9a345be55423523e893fb
JAROWMR <jarorutjes07@gmail.com>
2024-11-14 22:22:49 +0100

    saved reference in storage struct to send to handeler

--- src/crepe/system/CollisionSystem.h
+++ src/crepe/system/CollisionSystem.h
@@ -1,26 +1,30 @@
 #pragma once
 
 #include "Collider.h"
+#include "api/Rigidbody.h"
+#include "api/Transform.h"
+#include "api/BoxCollider.h"
+#include "api/CircleCollider.h"
+#include <tuple>
 #include <vector>
 #include <variant>
 
 namespace crepe {
 
-class Collider;
-class BoxCollider;
-class CircleCollider;
-class Transform;
-class Rigidbody;
 
 class CollisionSystem {
+private:
+	using collider_stor = std::variant<BoxCollider, CircleCollider>;
+private:
+	struct CollidedInfoStor {
+	std::tuple<collider_stor,Transform,Rigidbody> collider_info;
+};
 public:
 	CollisionSystem();
 	void update();
 private:
-	using collider_stor = std::variant<BoxCollider, CircleCollider>;
-private:
-	void call_collision_handler(const Rigidbody& rigidbody1,const Rigidbody& rigidbody2);
-  std::vector<std::pair<collider_stor, collider_stor>> check_collisions(const std::vector<std::reference_wrapper<BoxCollider>>& boxcolliders, const std::vector<std::reference_wrapper<CircleCollider>>& circlecolliders);
+	void call_collision_handler(const CollidedInfoStor& data1,const CollidedInfoStor& data2);
+  std::vector<std::pair<CollidedInfoStor,CollidedInfoStor>> check_collisions(const std::vector<std::reference_wrapper<BoxCollider>>& boxcolliders, const std::vector<std::reference_wrapper<CircleCollider>>& circlecolliders);
 	bool check_box_box_collision(const BoxCollider& box1, const BoxCollider& box2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
 	bool check_box_circle_collision(const BoxCollider& box1, const CircleCollider& circle2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
 	bool check_circle_circle_collision(const CircleCollider& circle1, const CircleCollider& circle2, const Transform& transform1, const Transform& transform2, const Rigidbody& rigidbody1, const Rigidbody& rigidbody2);
