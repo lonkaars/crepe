f58fc472c8188c3a4813f60a931395efbe64ec77
JAROWMR <jarorutjes07@gmail.com>
2024-11-19 20:43:53 +0100

    added doxygen comments

--- src/crepe/Collider.h
+++ src/crepe/Collider.h
@@ -1,13 +1,16 @@
 #pragma once
 
-#include "Component.h"
 #include "api/Vector2.h"
+
+#include "Component.h"
 
 namespace crepe {
 
 class Collider : public Component {
 public:
 	Collider(game_object_id_t id, Vector2 offset);
+
+	//! Offset of the collider relative to rigidbody position 
 	Vector2 offset;
 };
 
