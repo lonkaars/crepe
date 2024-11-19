30999a69293a3a15f7947f4023df4a753b92491d
JAROWMR <jarorutjes07@gmail.com>
2024-11-19 20:42:02 +0100

    doxygen comment colliders

--- src/crepe/api/BoxCollider.h
+++ src/crepe/api/BoxCollider.h
@@ -5,10 +5,19 @@
 
 namespace crepe {
 
+/**
+ * \brief A class representing a box-shaped collider.
+ * 
+ * This class is used for collision detection with other colliders (e.g., CircleCollider).
+ */
 class BoxCollider : public Collider {
 public:
 	BoxCollider(game_object_id_t game_object_id,Vector2 offset, double width, double height);
+
+	//! Width of box collider 
 	double width;
+
+	//! Height of box collider
 	double height;
 };
 
