2cf9c96b4cc843c89c9567c2321b240f95961622
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-19 15:05:30 +0100

    add RefVector

--- src/crepe/types.h
+++ src/crepe/types.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
+#include <functional>
 
 namespace crepe {
 
+//! GameObject ID
 typedef uint32_t game_object_id_t;
+
+//! vector of reference_wrapper
+template <typename T>
+using RefVector = std::vector<std::reference_wrapper<T>>;
 
 }
