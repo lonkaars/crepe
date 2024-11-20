e585e01b625fcdbc00709c1bbade1b542b1f6139
max-001 <maxsmits21@kpnmail.nl>
2024-11-20 11:42:23 +0100

    Added Doxygen comments/explanation for GameObjects and Scenes

--- src/crepe/api/Scene.h
+++ src/crepe/api/Scene.h
@@ -7,19 +7,33 @@ namespace crepe {
 class SceneManager;
 class ComponentManager;
 
+/**
+ * \brief Represents a Scene
+ * 
+ * This class represents a Scene. The Scene class is only used as an interface for the game
+ * programmer.
+ */
 class Scene {
 protected:
+	/**
+	 * \param mgr  Reference to the ComponentManager
+	 * \param name  Name of the scene
+	 */
 	Scene(ComponentManager & mgr, const std::string & name);
+	//! SceneManager instances Scene
 	friend class SceneManager;
 
 public:
 	virtual ~Scene() = default;
 
 public:
+	//! Load the scene
 	virtual void load_scene() = 0;
+	//! The scene name
 	const std::string name;
 
 protected:
+	//! Reference to the ComponentManager
 	ComponentManager & component_manager;
 };
 
