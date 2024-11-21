2d15d1dd9e50ccf6b9a9a96d8be308da9254dd49
max-001 <maxsmits21@kpnmail.nl>
2024-11-21 17:04:37 +0100

    Made Vector2 templated

---
+++ src/crepe/api/Vector2.hpp
@@ -0,0 +1,44 @@
+#include "Vector2.h"
+
+namespace crepe {
+
+template <class T>
+Vector2<T> Vector2<T>::operator-(const Vector2<T> & other) const { return {x - other.x, y - other.y}; }
+
+template <class T>
+Vector2<T> Vector2<T>::operator+(const Vector2<T> & other) const { return {x + other.x, y + other.y}; }
+
+template <class T>
+Vector2<T> Vector2<T>::operator*(double scalar) const { return {x * scalar, y * scalar}; }
+
+template <class T>
+Vector2<T> & Vector2<T>::operator*=(const Vector2<T> & other) {
+	x *= other.x;
+	y *= other.y;
+	return *this;
+}
+
+template <class T>
+Vector2<T> & Vector2<T>::operator+=(const Vector2<T> & other) {
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+template <class T>
+Vector2<T> & Vector2<T>::operator+=(double other) {
+	x += other;
+	y += other;
+	return *this;
+}
+
+template <class T>
+Vector2<T> Vector2<T>::operator-() const { return {-x, -y}; }
+
+template <class T>
+bool Vector2<T>::operator==(const Vector2<T> & other) const { return x == other.x && y == other.y; }
+
+template <class T>
+bool Vector2<T>::operator!=(const Vector2<T> & other) const { return !(*this == other); }
+
+} // namespace crepe
