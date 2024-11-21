db66bb4babb19e8a86d5c61281c8b94469729d03
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-21 10:52:22 +0100

    use OptionalRef instead of pointer references

--- src/crepe/system/ScriptSystem.cpp
+++ src/crepe/system/ScriptSystem.cpp
@@ -1,5 +1,3 @@
-#include <functional>
-
 #include "../ComponentManager.h"
 #include "../api/BehaviorScript.h"
 #include "../api/Script.h"
@@ -12,30 +10,20 @@ using namespace crepe;
 void ScriptSystem::update() {
 	dbg_trace();
 
-	RefVector<Script> scripts = this->get_scripts();
-
-	for (auto & script_ref : scripts) {
-		Script & script = script_ref.get();
-		if (!script.initialized) {
-			script.init();
-			script.initialized = true;
-		}
-		script.update();
-	}
-}
-
-RefVector<Script> ScriptSystem::get_scripts() const {
-	RefVector<Script> scripts = {};
 	ComponentManager & mgr = this->component_manager;
 	RefVector<BehaviorScript> behavior_scripts = mgr.get_components_by_type<BehaviorScript>();
 
-	for (auto behavior_script_ref : behavior_scripts) {
-		BehaviorScript & behavior_script = behavior_script_ref.get();
+	for (BehaviorScript & behavior_script : behavior_scripts) {
 		if (!behavior_script.active) continue;
+
 		Script * script = behavior_script.script.get();
 		if (script == nullptr) continue;
-		scripts.push_back(*script);
-	}
 
-	return scripts;
+		if (!script->initialized) {
+			script->init();
+			script->initialized = true;
+		}
+		script->update();
+	}
 }
+
