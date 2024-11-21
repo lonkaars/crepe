de45ac70a843879d0364ebbe55a086f0f14d321a
Loek Le Blansch <loek@pipeframe.xyz>
2024-11-21 09:29:15 +0100

    remove OptionalRef::set and OptionalRef::get

--- src/crepe/util/OptionalRef.hpp
+++ src/crepe/util/OptionalRef.hpp
@@ -8,35 +8,30 @@ namespace crepe {
 
 template <typename T>
 OptionalRef<T>::OptionalRef(T & ref) {
-	this->set(ref);
+	this->ref = &ref;
 }
 
 template <typename T>
-T & OptionalRef<T>::get() const {
+OptionalRef<T>::operator T & () const {
 	if (this->ref == nullptr)
 		throw std::runtime_error("OptionalRef: attempt to dereference nullptr");
 	return *this->ref;
 }
 
 template <typename T>
-void OptionalRef<T>::set(T & ref) noexcept {
-	this->ref = &ref;
-}
-
-template <typename T>
-void OptionalRef<T>::clear() noexcept {
-	this->ref = nullptr;
-}
-
-template <typename T>
 OptionalRef<T> & OptionalRef<T>::operator=(T & ref) {
-	this->set(ref);
+	this->ref = &ref;
 	return *this;
 }
 
 template <typename T>
 OptionalRef<T>::operator bool() const noexcept {
 	return this->ref != nullptr;
+}
+
+template <typename T>
+void OptionalRef<T>::clear() noexcept {
+	this->ref = nullptr;
 }
 
 } // namespace crepe
