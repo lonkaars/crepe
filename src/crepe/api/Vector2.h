a7e39f06b034b6f174997a0f62d93d47fbd64ad9
max-001 <maxsmits21@kpnmail.nl>
2024-11-22 08:52:00 +0100

    Added <T> to arguments

--- src/crepe/api/Vector2.h
+++ src/crepe/api/Vector2.h
@@ -11,49 +11,49 @@ struct Vector2 {
 	T y = 0;
 
 	//! Subtracts another vector from this vector and returns the result.
-	Vector2 operator-(const Vector2 & other) const;
+	Vector2 operator-(const Vector2<T> & other) const;
 
 	//! Subtracts a scalar value from both components of this vector and returns the result.
 	Vector2 operator-(T scalar) const;
 
 	//! Adds another vector to this vector and returns the result.
-	Vector2 operator+(const Vector2 & other) const;
+	Vector2 operator+(const Vector2<T> & other) const;
 
 	//! Adds a scalar value to both components of this vector and returns the result.
 	Vector2 operator+(T scalar) const;
 
 	//! Multiplies this vector by another vector element-wise and returns the result.
-	Vector2 operator*(const Vector2 & other) const;
+	Vector2 operator*(const Vector2<T> & other) const;
 
 	//! Multiplies this vector by a scalar and returns the result.
 	Vector2 operator*(T scalar) const;
 
 	//! Divides this vector by another vector element-wise and returns the result.
-	Vector2 operator/(const Vector2 & other) const;
+	Vector2 operator/(const Vector2<T> & other) const;
 
 	//! Divides this vector by a scalar and returns the result.
 	Vector2 operator/(T scalar) const;
 
 	//! Adds another vector to this vector and updates this vector.
-	Vector2 & operator+=(const Vector2 & other);
+	Vector2 & operator+=(const Vector2<T> & other);
 
 	//! Adds a scalar value to both components of this vector and updates this vector.
 	Vector2 & operator+=(T other);
 
 	//! Subtracts another vector from this vector and updates this vector.
-	Vector2 & operator-=(const Vector2 & other);
+	Vector2 & operator-=(const Vector2<T> & other);
 
 	//! Subtracts a scalar value from both components of this vector and updates this vector.
 	Vector2 & operator-=(T other);
 
 	//! Multiplies this vector by another vector element-wise and updates this vector.
-	Vector2 & operator*=(const Vector2 & other);
+	Vector2 & operator*=(const Vector2<T> & other);
 
 	//! Multiplies this vector by a scalar and updates this vector.
 	Vector2 & operator*=(T other);
 
 	//! Divides this vector by another vector element-wise and updates this vector.
-	Vector2 & operator/=(const Vector2 & other);
+	Vector2 & operator/=(const Vector2<T> & other);
 
 	//! Divides this vector by a scalar and updates this vector.
 	Vector2 & operator/=(T other);
@@ -62,10 +62,10 @@ struct Vector2 {
 	Vector2 operator-() const;
 
 	//! Checks if this vector is equal to another vector.
-	bool operator==(const Vector2 & other) const;
+	bool operator==(const Vector2<T> & other) const;
 
 	//! Checks if this vector is not equal to another vector.
-	bool operator!=(const Vector2 & other) const;
+	bool operator!=(const Vector2<T> & other) const;
 };
 
 } // namespace crepe
