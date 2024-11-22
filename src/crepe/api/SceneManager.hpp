c0b8c82a0f1655aabfa791a4c431acec1f3f6786
max-001 <maxsmits21@kpnmail.nl>
2024-11-22 09:36:35 +0100

    Replaced ComponentManager reference by OptionalRef for late binding

--- src/crepe/api/SceneManager.hpp
+++ src/crepe/api/SceneManager.hpp
@@ -4,12 +4,15 @@
 
 namespace crepe {
 
-template <typename T>
-void SceneManager::add_scene() {
+template <typename T, typename... Args>
+void SceneManager::add_scene(Args &&... args) {
 	using namespace std;
 	static_assert(is_base_of<Scene, T>::value, "T must be derived from Scene");
 
-	Scene * scene = new T(this->component_manager);
+	Scene * scene = new T(std::forward<args>(args)...);
+
+	scene->component_manager = this->component_manager;
+
 	this->scenes.emplace_back(unique_ptr<Scene>(scene));
 
 	// The first scene added, is the one that will be loaded at the beginning
