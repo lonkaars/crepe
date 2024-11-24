256cd7e07f493393a6c6f2946f47a0d79328b151
WBoerenkamps <wrj.boerenkamps@student.avans.nl>
2024-11-24 19:38:13 +0100

    loopTimer tests

--- src/test/loopTimerTest.cpp
+++ src/test/loopTimerTest.cpp
@@ -9,26 +9,24 @@ using namespace std;
 using namespace std::chrono_literals;
 using namespace crepe;
 
-class GameLoopTest : public ::testing::Test {
+class LoopTimerTest : public ::testing::Test {
 public:
-LoopManager loop_manager;
 LoopTimer loop_timer = LoopTimer::get_instance();
 protected:
 	void SetUp() override {
 		loop_timer.start();
-		loop_manager.start();
 	}
 
 	void TearDown() override {
 		
 	}
 };
-TEST_F(GameLoopTest, TestDeltaTime) {
+TEST_F(LoopTimerTest, TestDeltaTime) {
     auto start_time = std::chrono::steady_clock::now();
     
     loop_timer.update();  
     double delta_time = loop_timer.get_delta_time();
 
     auto elapsed_time = std::chrono::steady_clock::now() - start_time;
-    EXPECT_LE(delta_time, std::chrono::duration<double>(elapsed_time).count());  // delta_time should match or be slightly less
+    EXPECT_LE(delta_time, std::chrono::duration<double>(elapsed_time).count());
 }
