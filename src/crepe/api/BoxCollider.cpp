e8c83a6a4421ae81215cf8bd6393b0ff014f147e
JAROWMR <jarorutjes07@gmail.com>
2024-12-01 20:00:24 +0100

    feedback changes

--- src/crepe/api/BoxCollider.cpp
+++ src/crepe/api/BoxCollider.cpp
@@ -1,7 +1,7 @@
 #include "BoxCollider.h"
+
 #include "../Collider.h" 
 
 using namespace crepe;
-
 
 BoxCollider::BoxCollider(game_object_id_t game_object_id,vec2 offset, double width, double height) : Collider(game_object_id,offset), width(width), height(height) {}
