7f94c579cff348c35a419126840248237eb044d0
WBoerenkamps <wrj.boerenkamps@student.avans.nl>
2024-12-04 21:50:00 +0100

    feedback changes

--- src/crepe/api/UiObject.cpp
+++
@@ -1,8 +0,0 @@
-#include "UiObject.h"
-
-using namespace crepe;
-
-UIObject::UIObject(game_object_id_t id, vec2 dimensions, vec2 offset)
-	: Component(id),
-	  dimensions(dimensions),
-	  offset(offset) {}
