5ef66d784838dc5735898cb4db7ad6fbf3dd0c9f
max-001 <maxsmits21@kpnmail.nl>
2024-12-04 13:46:41 +0100

    Setup some behaviors

--- src/crepe/system/AISystem.cpp
+++ src/crepe/system/AISystem.cpp
@@ -1,6 +1,34 @@
+#include "../ComponentManager.h"
+
 #include "AISystem.h"
-#include <iostream>
 
 using namespace crepe;
 
-void AISystem::update() { std::cout << "AI System update" << std::endl; }
+void AISystem::update() {
+	ComponentManager & mgr = this->component_manager;
+	RefVector<AI> ai_components = mgr.get_components_by_type<AI>();
+
+	for (AI & ai : ai_components) {
+		vec2 force = this->calculate(ai);
+		//...
+	}
+}
+
+vec2 AISystem::calculate(AI & ai) {
+	vec2 force;
+
+	if (ai.on(AI::BehaviorType::SEEK)) {
+		// Seek the target
+	}
+	if (ai.on(AI::BehaviorType::FLEE)) {
+		// Flee from the target
+	}
+	if (ai.on(AI::BehaviorType::ARRIVE)) {
+		// Arrive at the target
+	}
+	if (ai.on(AI::BehaviorType::PATH_FOLLOW)) {
+		// Follow the path
+	}
+
+	return force;
+}
--- src/crepe/system/AISystem.h
+++ src/crepe/system/AISystem.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "api/AI.h"
+
 #include "System.h"
+#include "types.h"
 
 namespace crepe {
 
@@ -9,6 +12,9 @@ public:
 	using System::System;
 
 	void update() override;
+
+private:
+	vec2 calculate(AI & ai);
 };
 
 } // namespace crepe
