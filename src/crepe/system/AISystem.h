175969f5028d847a9df0782a80ad7e0301fdea45
max-001 <maxsmits21@kpnmail.nl>
2024-12-06 10:46:16 +0100

    Corrected accumulate_force()

--- src/crepe/system/AISystem.h
+++ src/crepe/system/AISystem.h
@@ -15,9 +15,12 @@ public:
 
 private:
 	vec2 calculate(AI & ai);
-	bool accumulate_force(vec2 & running_total, vec2 force_to_add);
+	bool accumulate_force(AI & ai, vec2 & running_total, vec2 force_to_add);
 
 	vec2 seek(const AI & ai);
+	vec2 flee(const AI & ai);
+	vec2 arrive(const AI & ai);
+	vec2 path_follow(const AI & ai);
 };
 
 } // namespace crepe
