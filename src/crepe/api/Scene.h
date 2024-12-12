72db89ee9f9b661996fc54e1280efe1816b1150a
JAROWMR <jarorutjes07@gmail.com>
2024-12-12 19:35:19 +0100

    shielded mediator

--- src/crepe/api/Scene.h
+++ src/crepe/api/Scene.h
@@ -3,12 +3,16 @@
 #include <string>
 
 #include "../manager/Mediator.h"
+#include "../manager/ResourceManager.h"
+#include "../manager/ComponentManager.h"
 #include "../util/OptionalRef.h"
+#include "GameObject.h"
 
 namespace crepe {
 
 class SceneManager;
 class ComponentManager;
+class Asset;
 
 /**
  * \brief Represents a Scene
@@ -38,7 +42,7 @@ public:
 	// TODO: Late references should ALWAYS be private! This is currently kept as-is so unit tests
 	// keep passing, but this reference should not be directly accessible by the user!!!
 
-protected:
+private:
 	/**
 	 * \name Late references
 	 *
@@ -51,8 +55,62 @@ protected:
 	 * \{
 	 */
 	//! Mediator reference
-	OptionalRef<Mediator> mediator;
 	//! \}
+	OptionalRef<Mediator> mediator;
+
+protected:
+
+	/**
+	* \brief Retrieve the reference to the SaveManager instance
+	*
+	* \returns A reference to the SaveManager instance held by the Mediator.
+	*/
+	SaveManager& get_save_manager() const{
+		return mediator->save_manager;
+	}
+
+	/**
+	 * \brief Create a new game object using the component manager
+	 *
+	 * \param name Metadata::name (required)
+	 * \param tag Metadata::tag (optional, empty by default)
+	 * \param position Transform::position (optional, origin by default)
+	 * \param rotation Transform::rotation (optional, 0 by default)
+	 * \param scale Transform::scale (optional, 1 by default)
+	 *
+	 * \returns GameObject interface
+	 *
+	 * \note This method automatically assigns a new entity ID
+	 */
+	GameObject new_object(const std::string & name, const std::string & tag = "",
+                          const vec2 & position = {0, 0}, double rotation = 0,
+                          double scale = 1) {
+		// Forward the call to ComponentManager's new_object method
+		return mediator->component_manager->new_object(name, tag, position, rotation, scale);
+	}
+
+	/**
+	 * \brief Mark a resource as persistent (i.e. used across multiple scenes)
+	 *
+	 * \param asset Asset the concrete resource is instantiated from
+	 * \param persistent Whether this resource is persistent (true=keep, false=destroy)
+	 */
+	void set_persistent(const Asset & asset, bool persistent){
+		mediator->resource_manager->set_persistent(asset, persistent);
+	}
+
+	/**
+	 * \brief Log a message using Log::logf
+	 *
+	 * \tparam Args Log::logf parameters
+	 * \param args  Log::logf parameters
+	 */
+	template <typename... Args>
+	void logf(Args &&... args);
+
 };
 
 } // namespace crepe
+
+
+#include "Scene.hpp"
