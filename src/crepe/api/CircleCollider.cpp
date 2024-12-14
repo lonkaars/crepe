4cddaf8e8b809a69a8e03e79c4f7902920745a9a
JAROWMR <jarorutjes07@gmail.com>
2024-12-14 18:23:31 +0100

    make format

--- src/crepe/api/CircleCollider.cpp
+++ src/crepe/api/CircleCollider.cpp
@@ -2,6 +2,7 @@
 
 using namespace crepe;
 
-CircleCollider::CircleCollider(game_object_id_t game_object_id, float radius, const vec2 & offset)
+CircleCollider::CircleCollider(game_object_id_t game_object_id, float radius,
+							   const vec2 & offset)
 	: Collider(game_object_id, offset),
 	  radius(radius) {}
