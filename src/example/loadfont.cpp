2ffdb8617e35c10fa39ca768e52c85756499a845
WBoerenkamps <wrj.boerenkamps@student.avans.nl>
2024-12-17 15:14:32 +0100

    feedback changes

--- src/example/loadfont.cpp
+++ src/example/loadfont.cpp
@@ -3,6 +3,7 @@
 #include <crepe/api/Text.h>
 #include <crepe/facade/Font.h>
 #include <crepe/facade/SDLContext.h>
+#include <crepe/facade/FontFacade.h>
 #include <crepe/manager/Mediator.h>
 #include <crepe/manager/ResourceManager.h>
 #include <exception>
@@ -14,15 +15,17 @@ int main() {
 
 	// SDLFontContext font_facade;
 	Mediator mediator;
+	FontFacade font_facade{};
 	SDLContext sdl_context{mediator};
 	// ComponentManager component_manager{mediator};
 	ResourceManager resource_manager{mediator};
 	try {
 		// Correct way to create a unique pointer for Text
 		std::unique_ptr<Text> label = std::make_unique<Text>(
-			1, vec2(100, 100), vec2(0, 0), "OpenSymbol", Text::Data{}, "test text", Asset(""));
+			1, vec2(100, 100), vec2(0, 0), "OpenSymbol", Text::Data{}, "test text");
 		// std::cout << "Path: " << label->font.get_path() << std::endl;
-
+		Asset asset1 = font_facade.get_font_asset("OpenSymbol");
+		std::cout << asset1.get_path() << std::endl;
 		std::unique_ptr<Text> label2 = std::make_unique<Text>(
 			1, vec2(100, 100), vec2(0, 0), "fsaafdafsdafsdafsdasfdds", Text::Data{});
 		Asset asset = Asset("test test");
