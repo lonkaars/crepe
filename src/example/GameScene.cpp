f7b8062d6a33195bfc43af0f2a7b6c16a388b588
max-001 <maxsmits21@kpnmail.nl>
2024-12-17 13:41:14 +0100

    Created file for Player

--- src/example/GameScene.cpp
+++ src/example/GameScene.cpp
@@ -1,12 +1,12 @@
 #include "GameScene.h"
 #include "Background.h"
-#include "api/Animator.h"
-#include "api/Asset.h"
-#include "api/BoxCollider.h"
-#include "api/Transform.h"
+#include "Player.h"
 
 #include <cmath>
+#include <crepe/api/Animator.h>
+#include <crepe/api/Asset.h>
 #include <crepe/api/BehaviorScript.h>
+#include <crepe/api/BoxCollider.h>
 #include <crepe/api/Camera.h>
 #include <crepe/api/Color.h>
 #include <crepe/api/Event.h>
@@ -14,6 +14,7 @@
 #include <crepe/api/Rigidbody.h>
 #include <crepe/api/Script.h>
 #include <crepe/api/Sprite.h>
+#include <crepe/api/Transform.h>
 #include <crepe/types.h>
 #include <iostream>
 
@@ -87,24 +88,6 @@ private:
 	bool took_jetpack = false;
 };
 
-class PlayerScript : public Script {
-public:
-	void init() {
-		subscribe<KeyPressEvent>(
-			[this](const KeyPressEvent & ev) -> bool { return this->keypressed(ev); });
-	}
-
-private:
-	bool keypressed(const KeyPressEvent & event) {
-		if (event.key == Keycode::SPACE) {
-			Rigidbody & rb = this->get_components_by_name<Rigidbody>("player").front();
-			rb.add_force_linear(vec2(0, -10));
-			return true;
-		}
-		return false;
-	}
-};
-
 void GameScene::load_scene() {
 	Background background(*this);
 
@@ -114,57 +97,9 @@ void GameScene::load_scene() {
 									 .bg_color = Color::RED,
 								 });
 	camera.add_component<BehaviorScript>().set_script<MoveCameraScript>();
-	camera.add_component<Rigidbody>(Rigidbody::Data{
-		.linear_velocity = vec2(0, 0),
-	});
+	camera.add_component<Rigidbody>(Rigidbody::Data{});
 
-	GameObject player = new_object("player", "player", vec2(-100, 200));
-	Asset player_body_asset{"asset/jetpack_joyride/barry/defaultBody.png"};
-	Sprite & player_body_sprite
-		= player.add_component<Sprite>(player_body_asset, Sprite::Data{
-															  .sorting_in_layer = 10,
-															  .order_in_layer = 0,
-															  .size = vec2(0, 50),
-														  });
-	player.add_component<Animator>(player_body_sprite, ivec2(32, 32), uvec2(4, 8),
-								   Animator::Data{
-									   .fps = 5,
-									   .looping = true,
-								   });
-	Asset player_head_asset{"asset/jetpack_joyride/barry/defaultHead.png"};
-	Sprite & player_head_sprite
-		= player.add_component<Sprite>(player_head_asset, Sprite::Data{
-															  .sorting_in_layer = 10,
-															  .order_in_layer = 1,
-															  .size = vec2(0, 50),
-															  .position_offset = vec2(0, -20),
-														  });
-	player.add_component<Animator>(player_head_sprite, ivec2(32, 32), uvec2(4, 8),
-								   Animator::Data{
-									   .fps = 5,
-									   .looping = true,
-								   });
-	Asset player_jetpack_asset{"asset/jetpack_joyride/barry/jetpackDefault.png"};
-	Sprite & player_jetpack_sprite = player.add_component<Sprite>(
-		player_jetpack_asset, Sprite::Data{
-								  .sorting_in_layer = 10,
-								  .order_in_layer = 2,
-								  .size = vec2(0, 60),
-								  .position_offset = vec2(-20, 0),
-							  });
-	player_jetpack_sprite.active = false;
-	player.add_component<Animator>(player_jetpack_sprite, ivec2(32, 44), uvec2(4, 4),
-								   Animator::Data{
-									   .fps = 5,
-									   .looping = true,
-								   });
-	player.add_component<Rigidbody>(Rigidbody::Data{
-		.gravity_scale = 10,
-		.body_type = Rigidbody::BodyType::DYNAMIC,
-		.linear_velocity = vec2(100, 0),
-	});
-	player.add_component<BoxCollider>(vec2(50, 50));
-	player.add_component<BehaviorScript>().set_script<PlayerScript>().active = false;
+	Player player(*this);
 
 	GameObject floor = new_object("floor", "game_world", vec2(0, 325));
 	floor.add_component<Rigidbody>(Rigidbody::Data{
