0865bfdb33e94f3d86d81a226232dee0fca4efda
Loek Le Blansch <loek@pipeframe.xyz>
2024-12-18 14:23:34 +0100

    update ScriptSystem doxygen

--- src/crepe/system/ScriptSystem.h
+++ src/crepe/system/ScriptSystem.h
@@ -11,31 +11,26 @@ class Script;
 /**
  * \brief Script system
  *
- * The script system is responsible for all \c BehaviorScript components, and
- * calls the methods on classes derived from \c Script.
+ * The script system is responsible for all \c BehaviorScript components, and calls the methods
+ * on classes derived from \c Script.
  */
 class ScriptSystem : public System {
 public:
 	using System::System;
-	/**
-	 * \brief Call Script::fixed_update() on all active \c BehaviorScript instances
-	 *
-	 * This routine updates all scripts sequentially using the Script::update()
-	 * method. It also calls Script::init() if this has not been done before on
-	 * the \c BehaviorScript instance.
-	 */
-	void fixed_update() override;
 
-	/**
-	 * \brief Call Script::frame_update() on all active \c BehaviorScript instances
-	 *
-	 * This routine updates all scripts sequentially using the Script::update()
-	 * method. It also calls Script::init() if this has not been done before on
-	 * the \c BehaviorScript instance.
-	 */
+public:
+	//! Call Script::fixed_update() on all active \c BehaviorScript instances
+	void fixed_update() override;
+	//! Call Script::frame_update() on all active \c BehaviorScript instances
 	void frame_update() override;
 
 private:
+	/**
+	 * \brief Call Script `*_update` member function on all active \c BehaviorScript instances
+	 *
+	 * \note This routine also calls Script::init() if this has not been done before on the \c
+	 * BehaviorScript instance.
+	 */
 	void update(void (Script::*update_function)(duration_t), const duration_t & delta_time);
 };
 
