5f08d1482057f658344481ad3f26b6d2488b9c44
heavydemon21 <nielsstunnebrink1@gmail.com>
2024-12-19 13:52:28 +0100

    coin class

--- src/example/PreviewScenes/Coin.cpp
+++ src/example/PreviewScenes/Coin.cpp
@@ -2,7 +2,8 @@
 #include "Coin.h"
 #include "api/Asset.h"
 #include "api/AudioSource.h"
-#include "example/PreviewScenes/Scene1.h"
+#include "api/BehaviorScript.h"
+#include "system/CollisionSystem.h"
 #include "types.h"
 
 #include <crepe/api/Animator.h>
@@ -10,19 +11,33 @@
 #include <crepe/api/Scene.h>
 #include <crepe/api/Script.h>
 #include <crepe/api/Sprite.h>
+#include <iostream>
 
 using namespace crepe;
 using namespace std;
 
-Coin::Coin(Scene1 & s, const vec2 & start_pos) : scene(s) {
-	Asset coin_ss{"assets/Entities/coin1_TVOS.png"};
-	Asset coin_pickup_audio{"assets/SFX/Obstacle/Coin/coin_pickup_1.mp3"};
+class CoinPickup : public Script {
+	bool on_collision(const CollisionEvent & ev) {
+		const auto & info = ev.info;
+		cout << info.this_metadata.game_object_id << " " << info.other_metadata.name << endl;
+		return true;
+	}
+	void init() {
+		subscribe<CollisionEvent>(
+			[this](const CollisionEvent & ev) -> bool { return this->on_collision(ev); });
+	}
+};
+
+Coin::Coin(GameObject & coin, const vec2 & start_pos) {
+	Asset asset_coin{"assets/Entities/coin1_TVOS.png"};
+	Asset asset_coin_pickup_audio{"assets/SFX/Obstacle/Coin/coin_pickup_1.mp3"};
 
 	vec2 size = {20, 20};
-	GameObject coin = this->scene.create_object("coin", "coin", start_pos, 0, 1);
-	this->coin_sprite = coin.add_component<Sprite>(coin_ss, Sprite::Data{
-																 .size = size,
-															 });
+	this->coin_sprite = coin.add_component<Sprite>(asset_coin, Sprite::Data{
+																   .sorting_in_layer = 10,
+																   .order_in_layer = 10,
+																   .size = size,
+															   });
 
 	coin.add_component<Rigidbody>(Rigidbody::Data{
 		.mass = 10,
@@ -32,14 +47,15 @@ Coin::Coin(Scene1 & s, const vec2 & start_pos) : scene(s) {
 	});
 
 	coin.add_component<CircleCollider>(size.x / 2);
-	coin.add_component<Animator>(coin_sprite, ivec2{32, 32}, uvec2{8, 1},
+	coin.add_component<Animator>(this->coin_sprite, ivec2{32, 32}, uvec2{8, 1},
 								 Animator::Data{
 									 .looping = true,
 								 });
-	this->coin_pickup_audio = coin.add_component<AudioSource>(coin_pickup_audio);
+	this->coin_pickup_audio = coin.add_component<AudioSource>(asset_coin_pickup_audio);
+	coin.add_component<BehaviorScript>().set_script<CoinPickup>();
 }
-	
-void Coin::pickup(){
-	this->coin_sprite->active = false;
-	this->coin_pickup_audio->play();
+
+void Coin::pickup() {
+	//this->coin_sprite->active = false;
+	//this->coin_pickup_audio->play();
 }
