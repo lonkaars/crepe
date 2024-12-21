799a376aeecd9bdc712e89324cbe9153206839c9
Max-001 <maxsmits21@kpnmail.nl>
2024-12-21 11:38:19 +0100

    Added comments

--- game/Config.h
+++ game/Config.h
@@ -1,11 +1,9 @@
 #pragma once
 
-static constexpr int SORT_IN_LAY_BACK_BACKGROUND = 3;
-static constexpr int SORT_IN_LAY_BACKGROUND = 4;
-static constexpr int SORT_IN_LAY_FORE_BACKGROUND = 5;
-static constexpr int SORT_IN_LAY_PARTICLES_BACKGROUND = 6;
-static constexpr int SORT_IN_LAY_OBSTACLES = 8;
-static constexpr int SORT_IN_LAY_PLAYER = 10;
-static constexpr int SORT_IN_LAY_PARTICLES_FOREGROUND = 15;
-
-
+static constexpr int SORT_IN_LAY_BACK_BACKGROUND = 3; // For all scenes
+static constexpr int SORT_IN_LAY_BACKGROUND = 4; // For all scenes
+static constexpr int SORT_IN_LAY_FORE_BACKGROUND = 5; // For all scenes
+static constexpr int SORT_IN_LAY_PARTICLES_BACKGROUND = 6; // For all scenes
+static constexpr int SORT_IN_LAY_OBSTACLES = 8; // Only for GameScene
+static constexpr int SORT_IN_LAY_PLAYER = 10; // Only for GameScene
+static constexpr int SORT_IN_LAY_PARTICLES_FOREGROUND = 15; // Only for GameScene
