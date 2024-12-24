295e903f240612517accde8b091a902356ee3639
Max-001 <maxsmits21@kpnmail.nl>
2024-12-24 12:10:28 +0100

    Added collision animations

--- game/player/PlayerScript.h
+++ game/player/PlayerScript.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <crepe/api/Event.h>
 #include <crepe/api/Script.h>
 
 class PlayerScript : public crepe::Script {
 public:
+	void init();
 	void fixed_update(crepe::duration_t dt);
+
+private:
+	bool on_collision(const crepe::CollisionEvent & ev);
 
 private:
 	int prev_anim = 0;
