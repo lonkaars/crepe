ac4ed92d12c80a36ba78f9477326e2521f0c1555
Max-001 <maxsmits21@kpnmail.nl>
2024-12-24 12:23:15 +0100

    Slowed down at end

--- game/EndGameScript.cpp
+++ game/EndGameScript.cpp
@@ -1,11 +1,20 @@
 #include "EndGameScript.h"
 
+#include <crepe/api/Rigidbody.h>
+#include <crepe/types.h>
+
+using namespace crepe;
+using namespace std;
+
 void EndGameScript::init() {
-	logf("EndGameScript::init");
-	// ...
+	Rigidbody & rb_player = this->get_components_by_name<Rigidbody>("player").front();
+	Rigidbody & rb_camera = this->get_components_by_name<Rigidbody>("camera").front();
+
+	rb_player.data.linear_velocity_coefficient = vec2(0.8, 0.8);
+	rb_camera.data.linear_velocity_coefficient = vec2(0.8, 0.8);
 }
 
 void EndGameScript::fixed_update(crepe::duration_t dt) {
-	logf("EndGameScript::fixed_update");
+	//logf("EndGameScript::fixed_update");
 	// ...
 }
