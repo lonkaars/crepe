fb48d70e0d2944317467f2b13cf04c014644b64e
Max-001 <maxsmits21@kpnmail.nl>
2024-12-24 12:17:32 +0100

    Added init

--- game/EndGameScript.cpp
+++ game/EndGameScript.cpp
@@ -1,5 +1,10 @@
 #include "EndGameScript.h"
 
+void EndGameScript::init() {
+	logf("EndGameScript::init");
+	// ...
+}
+
 void EndGameScript::fixed_update(crepe::duration_t dt) {
 	logf("EndGameScript::fixed_update");
 	// ...
