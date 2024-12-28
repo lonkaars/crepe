e007e8f467349eeddaf503e31897d4135ba6fed4
JAROWMR <jarorutjes07@gmail.com>
2024-12-28 20:46:33 +0100

    added presets

--- game/coins/CoinSystemScript.h
+++ game/coins/CoinSystemScript.h
@@ -15,9 +15,31 @@ private:
 	void add_location(const crepe::vec2& location);
 	void despawn_coins();
 	void spawn_coins();
+	float preset_1(const crepe::vec2 & begin_position);
+	float preset_2(const crepe::vec2 & begin_position);
 private:
 	static constexpr float SPAWN_DISTANCE = 400;
 	static constexpr float DESPAWN_DISTANCE = 400;
 	static constexpr float SPAWN_AREA = 50;
 	static std::vector<CoinData> coin_locations;
+private:
+// preset one settings
+// *****				*****
+//
+//
+//
+//				*****				*****
+	static constexpr float ROW_OFFSET_1 = 100;
+	static constexpr float COLUM_OFFSET_1 = 25;
+	static constexpr int COLUM_AMOUNT_1 = 5;
+private:
+// preset two settings
+// *****				*****
+//
+//
+//
+//				*****				*****
+	static constexpr float ROW_OFFSET_2 = 25;
+	static constexpr float COLUM_OFFSET_2 = 25;
+	static constexpr int COLUM_AMOUNT_2 = 10;
 };
