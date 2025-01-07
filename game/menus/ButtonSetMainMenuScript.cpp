a1c7b6ab92671f1df258e556dec06cda58d8103e
JAROWMR <jarorutjes07@gmail.com>
2025-01-07 14:32:43 +0100

    audio stopped at end of level

--- game/menus/ButtonSetMainMenuScript.cpp
+++ game/menus/ButtonSetMainMenuScript.cpp
@@ -1,5 +1,7 @@
 #include "ButtonSetMainMenuScript.h"
 #include "MenusConfig.h"
+#include "api/AudioSource.h"
+#include "types.h"
 
 using namespace crepe;
 using namespace std;
@@ -10,6 +12,12 @@ void ButtonSetMainMenuScript::init(){
 }
 
 bool ButtonSetMainMenuScript::on_button_press(const ButtonPressEvent& e){
+	RefVector<AudioSource> audios = this->get_components_by_name<AudioSource>("background_music");
+	
+	for (AudioSource & audio : audios) {
+		audio.stop();
+	}
+
 	this->set_next_scene(MAINMENU_SCENE);
 	return false;
 }
