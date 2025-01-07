ae581fa0de171747b49184779a2326fb96c48a7b
Max-001 <maxsmits21@kpnmail.nl>
2025-01-07 14:42:11 +0100

    Added Script suffix

--- game/workers/PanicFromPlayerScript.h
+++ game/workers/PanicFromPlayerScript.h
@@ -2,7 +2,7 @@
 
 #include <crepe/api/Script.h>
 
-class PanicFromPlayer : public crepe::Script {
+class PanicFromPlayerScript : public crepe::Script {
 public:
 	void fixed_update(crepe::duration_t dt);
 };
