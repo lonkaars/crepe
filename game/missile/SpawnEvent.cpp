8851106d1bb85f5683f7a698d54d06c8f7986c40
heavydemon21 <nielsstunnebrink1@gmail.com>
2025-01-07 19:24:07 +0100

    made code cleaner

--- game/missile/SpawnEvent.cpp
+++ game/missile/SpawnEvent.cpp
@@ -1,7 +1,6 @@
 #include "SpawnEvent.h"
-#include "api/AI.h"
-#include "api/AudioSource.h"
 
+#include <crepe/api/AudioSource.h>
 #include <crepe/api/Animator.h>
 #include <crepe/api/BehaviorScript.h>
 #include <crepe/api/Camera.h>
@@ -23,25 +22,29 @@ void MissileSpawnEventHandler::init() {
 std::random_device rd;
 std::mt19937 gen(rd());
 
-bool MissileSpawnEventHandler::on_event(const MissileSpawnEvent & ev) {
-	auto ms = this->get_components_by_name<Sprite>("missile");
-	auto mt = this->get_components_by_name<Transform>("missile");
-	auto mbs = this->get_components_by_name<BehaviorScript>("missile");
-	auto mas = this->get_components_by_name<AudioSource>("missile");
-	auto & cam = this->get_components_by_name<Transform>("camera").front().get();
+bool MissileSpawnEventHandler::on_event(const MissileSpawnEvent & event) {
+    auto missile_sprites = this->get_components_by_name<Sprite>("missile");
+    auto missile_transforms = this->get_components_by_name<Transform>("missile");
+    auto missile_behaviorscripts = this->get_components_by_name<BehaviorScript>("missile");
+    auto missile_audiosources = this->get_components_by_name<AudioSource>("missile");
+    auto & camera_transform = this->get_components_by_name<Transform>("camera").front().get();
 
-	for (int i = 0; i < mbs.size(); i++) {
-		if (!mbs[i].get().active) {
-			mbs[i].get().active = true;
+    for (size_t i = 0; i < missile_behaviorscripts.size(); ++i) {
+        auto & script = missile_behaviorscripts[i].get();
+        if (!script.active) {
+            script.active = true;
 
-			mas[i*2].get().play(true);
+            missile_audiosources[i * 2].get().play(true);
 
-			mt[i].get().position.x = cam.position.x + this->MISSILE_OFFSET;
-			std::uniform_int_distribution<> dist(this->MIN_RANGE,this->MAX_RANGE);
-			mt[i].get().position.y = dist(gen);
-			break;
-		}
-	}
+            auto & transform = missile_transforms[i].get();
+            transform.position.x = camera_transform.position.x + this->MISSILE_OFFSET;
 
-	return false;
+            std::uniform_int_distribution<> dist(this->MIN_RANGE, this->MAX_RANGE);
+            transform.position.y = dist(gen);
+
+            break;
+        }
+    }
+
+    return false;
 }
