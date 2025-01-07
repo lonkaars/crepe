a7bb9c455f8ebd197d1651d85efebd18f5ac971f
Max-001 <maxsmits21@kpnmail.nl>
2025-01-07 15:10:25 +0100

    Added the whole background

--- game/background/BackgroundSubScene.cpp
+++ game/background/BackgroundSubScene.cpp
@@ -34,4 +34,50 @@ BackgroundSubScene::BackgroundSubScene(Scene & scn) {
 	begin_x = hallway.create(scn, begin_x, 4, Color::GREEN);
 
 	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 5, Color::RED);
+
+	begin_x = forest.create(scn, begin_x, "3");
+
+	begin_x = hallway.create(scn, begin_x, 6, Color::BLUE);
+
+	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 7, Color::WHITE);
+
+	begin_x = forest.create(scn, begin_x, "4");
+
+	begin_x = hallway.create(scn, begin_x, 8, Color::YELLOW);
+
+	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 9, Color::MAGENTA);
+
+	begin_x = forest.create(scn, begin_x, "5");
+
+	begin_x = hallway.create(scn, begin_x, 10, Color::CYAN);
+
+	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 11, Color::GREEN);
+
+	begin_x = forest.create(scn, begin_x, "6");
+
+	begin_x = hallway.create(scn, begin_x, 12, Color::RED);
+
+	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 13, Color::BLUE);
+
+	begin_x = forest.create(scn, begin_x, "7");
+
+	begin_x = hallway.create(scn, begin_x, 14, Color::WHITE);
+
+	begin_x = aquarium.create(scn, begin_x);
+
+	begin_x = hallway.create(scn, begin_x, 15, Color::YELLOW);
+
+	begin_x = forest.create(scn, begin_x, "8");
+
+	begin_x = hallway.create(scn, begin_x, 16, Color::MAGENTA);
 }
