ad5fb53986fcc3f3b3c5369574e0f8e95051f3d9
Loek Le Blansch <loek@pipeframe.xyz>
2025-01-08 09:55:31 +0100

    `make format`

--- game/menus/ButtonSetShopSubScript.h
+++ game/menus/ButtonSetShopSubScript.h
@@ -8,6 +8,7 @@ class ButtonSetShopSubScript : public IButtonScript {
 public:
 	void init() override;
 	bool on_button_press(const crepe::ButtonPressEvent & e);
+
 protected:
 	bool transition = false;
 };
