b8ff1dcd4e531c3c858ee09f705cf553e86ed7ff
Max-001 <maxsmits21@kpnmail.nl>
2025-01-08 09:07:46 +0100

    Adjusted script

--- game/workers/CollisionScript.cpp
+++ game/workers/CollisionScript.cpp
@@ -1,9 +1,6 @@
 #include "CollisionScript.h"
 
 #include <crepe/api/Animator.h>
-#include <crepe/api/AudioSource.h>
-#include <crepe/api/ParticleEmitter.h>
-
 
 using namespace crepe;
 using namespace std;
@@ -15,18 +12,13 @@ void CollisionScript::init() {
 }
 
 bool CollisionScript::on_collision(const CollisionEvent & ev) {
-	RefVector<Animator> animators = this->get_components_by_name<Animator>("player");
-	RefVector<ParticleEmitter> emitters
-		= this->get_components_by_name<ParticleEmitter>("player");
+	RefVector<Animator> animators = this->get_components<Animator>();
 
 	if (ev.info.other.metadata.tag == "zapper") {
 		for (Animator & anim : animators) {
 			anim.active = true;
 			anim.set_anim(4);
 			anim.data.looping = true;
-		}
-		for (ParticleEmitter & emitter : emitters) {
-			emitter.data.emission_rate = 0;
 		}
 
 		return true;
@@ -36,11 +28,6 @@ bool CollisionScript::on_collision(const CollisionEvent & ev) {
 			anim.set_anim(4);
 			anim.data.looping = true;
 		}
-		for (ParticleEmitter & emitter : emitters) {
-			emitter.data.emission_rate = 0;
-		}
-		play_scr.active = false;
-		end_scr.active = true;
 
 		return true;
 	} else if (ev.info.other.metadata.tag == "missile") {
@@ -49,11 +36,6 @@ bool CollisionScript::on_collision(const CollisionEvent & ev) {
 			anim.set_anim(5);
 			anim.data.looping = true;
 		}
-		for (ParticleEmitter & emitter : emitters) {
-			emitter.data.emission_rate = 0;
-		}
-		play_scr.active = false;
-		end_scr.active = true;
 
 		return true;
 	}
