4a435b8fe98c5db1ecd95619997af10146135e85
Loek Le Blansch <loek@pipeframe.xyz>
2025-01-09 17:33:00 +0100

    improve player responsiveness

--- game/player/PlayerScript.h
+++ game/player/PlayerScript.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "util/OptionalRef.h"
 #include <chrono>
+#include <crepe/api/Config.h>
 #include <crepe/api/Event.h>
 #include <crepe/api/Script.h>
+
 class PlayerScript : public crepe::Script {
 public:
 	void init();
@@ -10,8 +13,11 @@ public:
 
 private:
 	bool on_collision(const crepe::CollisionEvent & ev);
+	bool on_key_down(const crepe::KeyPressEvent & ev);
+	bool on_key_up(const crepe::KeyReleaseEvent & ev);
 	// bool on_key_up(const crepe::KeyReleaseEvent& ev);
 	void shoot(const crepe::vec2 & location, float angle);
+	void help_kick(const crepe::vec2 & direction);
 
 private:
 	int prev_anim = 0;
@@ -19,4 +25,7 @@ private:
 	std::chrono::duration<float> shot_delay = std::chrono::duration<float>(0.5);
 
 	int current_jetpack_sound = 0;
+
+	float & engine_gravity = crepe::Config::get_instance().physics.gravity;
+	crepe::OptionalRef<crepe::Rigidbody> body;
 };
